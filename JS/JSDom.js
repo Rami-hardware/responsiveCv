2b5423d7ddcfdc7343cfe31eddfd9b0db3528186
[rami Dannah] <[killx51@hotmail.com]>
2019-01-13 22:34:06 +0300

    need fix in some Phones and applying JS Codes

--- JS/JSDom.js
+++ JS/JSDom.js
@@ -1,4 +1,8 @@
-$('.flip').hover(function(){
-        $(this).find('.card').toggleClass('flipped');
+var time = setInterval(Time, 1000);
 
- });
+function Time() {
+	var x = new Date();
+	document.getElementById("time").innerHTML = x.toLocaleTimeString();
+	document.getElementById("date").innerHTML = x.toDateString();
+	
+}
