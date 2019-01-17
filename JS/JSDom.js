a1940e2bee693a7e3935415a385d5fc301ae5f8c
Rami Dannah <killx51@hotmail.com>
2019-01-17 18:05:54 +0300

    Styling by JavaScript appling JS DOM

--- JS/JSDom.js
+++ JS/JSDom.js
@@ -1,8 +1,17 @@
-var time = setInterval(Time, 1000);
-
 function Time() {
-	var x = new Date();
-	document.getElementById("time").innerHTML = x.toLocaleTimeString();
-	document.getElementById("date").innerHTML = x.toDateString();
+	var tim = new Date();
+	document.getElementById("time").innerHTML = tim.toLocaleTimeString();
+	document.getElementById("date").innerHTML = tim.toDateString();
 	
 }
+var time = setInterval(Time, 1000);
+
+
+
+var myTite = document.querySelector("img");
+
+myTite.setAttribute("title", "proflie picture");
+
+
+document.getElementById("textH").style.textTransform = "uppercase";
+document.getElementById("textC").style.textTransform = "uppercase";
