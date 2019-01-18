7901ded7a64f99defd3144ca09347a8b04a118d8
[rami Dannah] <[killx51@hotmail.com]>
2019-01-18 15:43:28 +0300

    java

--- JS/JSDom.js
+++ JS/JSDom.js
@@ -1,8 +1,11 @@
+Math.PI; 
 var time = setInterval(Time, 1000);
-
 function Time() {
 	var x = new Date();
 	document.getElementById("time").innerHTML = x.toLocaleTimeString();
 	document.getElementById("date").innerHTML = x.toDateString();
-	
+
 }
+var addArt = document.querySelector("img");
+addArt.setAttribute('title', 'profile picture'); 
+
