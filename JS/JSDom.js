55879a09f38e1f13bd2aeacbd0383c5fa8a5412c
[rami Dannah] <[killx51@hotmail.com]>
2019-05-03 02:47:01 +0300

    change habbits to achievements

--- JS/JSDom.js
+++ JS/JSDom.js
@@ -1,31 +1,16 @@
-<<<<<<< HEAD
-Math.PI; 
-var time = setInterval(Time, 1000);
+//make clock in C.V
+var time = setInterval(Time(), 1000);
+
 function Time() {
 	var x = new Date();
 	document.getElementById("time").innerHTML = x.toLocaleTimeString();
 	document.getElementById("date").innerHTML = x.toDateString();
-
 }
-var addArt = document.querySelector("img");
-addArt.setAttribute('title', 'profile picture'); 
-
-=======
-function Time() {
-	var tim = new Date();
-	document.getElementById("time").innerHTML = tim.toLocaleTimeString();
-	document.getElementById("date").innerHTML = tim.toDateString();
-	
-}
-var time = setInterval(Time, 1000);
-
-
-
 var myTite = document.querySelector("img");
-
+//set attribute in image
 myTite.setAttribute("title", "proflie picture");
-
-
-document.getElementById("textH").style.textTransform = "uppercase";
-document.getElementById("textC").style.textTransform = "uppercase";
->>>>>>> 35df658ba22e90d21a5f857beeb6ba1c9e7a1cda
+var allHeadrsNames = document.querySelector("h1");
+//changing the text
+allHeadrsNames.setAttribute("title", "proflie picture");
+document.getElementById("H").style.textTransform = "uppercase";
+document.getElementById("H").setAttribute("title", "HTML");
