7508245e0acc513a485abd713a005adaf7e475b5
[rami Dannah] <[killx51@hotmail.com]>
2019-11-05 22:01:34 +0300

    rami

--- JS/JSDom.js
+++ JS/JSDom.js
@@ -1,11 +1,4 @@
 //make clock in C.V
-var time = setInterval(Time(), 1000);
-
-function Time() {
-	var x = new Date();
-	document.getElementById("time").innerHTML = x.toLocaleTimeString();
-	document.getElementById("date").innerHTML = x.toDateString();
-}
 var myTite = document.querySelector("img");
 //set attribute in image
 myTite.setAttribute("title", "proflie picture");
---
+++ public/JS/JSDom.js
@@ -0,0 +1,10 @@
+//make clock in C.V
+var myTite = document.querySelector("img");
+//set attribute in image
+myTite.setAttribute("title", "proflie picture");
+var allHeadrsNames = document.querySelector("h1");
+//changing the text
+allHeadrsNames.setAttribute("title", "proflie picture");
+document.getElementById("upper").style.textTransform = "uppercase";
+
+
